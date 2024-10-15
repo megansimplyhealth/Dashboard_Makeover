a2bd404458ee24e7e4c15f35f73974772070a286
Megan Knaresborough <Megan.Knaresborough@simplyhealth.co.uk>
2024-10-15 12:25:47 +0100

    banner temp pictures

--- pages/dashboardHome.tsx
+++ pages/dashboardHome.tsx
@@ -8,16 +8,15 @@ export default function DashboardHome() {
       <div className="bg-apple-green-20 min-h-screen">
         
         {/* header */}
-        <div className="absolute bg-white text-leaf-green px-10 py-1 z-10 w-full">
+        <div className="absolute text-leaf-green px-10 py-1 z-10 w-full">
           <div className="flex items-center justify-between py-2 text-5x1">
             <div className="flex ">
             <Image
-              src="/images/denplan-logo.jpg"
+              src="/images/Denplan-logo.png"
               alt="Denplan Logo"
-              width={131}
+              width={135}
               height={50}
             />
-
             </div>
             <div className="flex items-center text-gray-500">
               <span className="p-2 mr-96">navigation</span>
@@ -42,8 +41,22 @@ export default function DashboardHome() {
             </div>
           </div>
 
-          <div className="rounded-xl col-span-3 bg-apple-green-60 h-32 flex items-center justify-center mr-16 mt-1">
-            IMAGE OF EVENTS
+          <div className="rounded-xl col-span-3 h-32 flex items-center justify-center mr-16 mt-1">
+          <Image
+              src="/images/denplan-bike.png"
+              alt="Denplan Summit 2025 Event Banner"
+              width={650}
+              height={50}
+              className="rounded-xl mr-5 border border-grey"
+            />
+            <Image
+              src="/images/DenplanSummit2025.png"
+              alt="Denplan Summit 2025 Event Banner"
+              width={650}
+              height={50}
+              className="rounded-xl ml-5 border border-grey"
+            />
+            
           </div>
         </div>
 
@@ -60,24 +73,24 @@ export default function DashboardHome() {
         <div className="flex justify-center items-center flex-row pt-80 px-10 pb-4">         
           <div className="w-10/12">
             <div className="flex flex-row">
-              <div className="bg-no-repeat bg-red-200 border border-red-300 rounded-xl w-7/12 mr-2 p-6" >
-                <p className="text-5xl text-indigo-900">APP<br/><strong>ONE</strong></p>
-                <span className="bg-red-300 text-xl text-white inline-block rounded-full mt-12 px-8 py-2"><strong>CLICK</strong></span>
+              <div className="bg-no-repeat border border-leaf-green rounded-xl w-7/12 mr-2 p-6" >
+                <p className="text-5xl text-leaf-green-60">APP<br/><strong>ONE</strong></p>
+                <span className="bg-red-300 text-xl text-leaf-green inline-block rounded-full mt-12 px-8 py-2"><strong>CLICK</strong></span>
               </div>
 
-              <div className="bg-no-repeat bg-orange-200 border border-orange-300 rounded-xl w-5/12 ml-2 p-6" >
-                <p className="text-5xl text-indigo-900">APP <br/><strong>TWO</strong></p>
-                <a href="" className="bg-orange-300 text-xl text-white underline hover:no-underline inline-block rounded-full mt-12 px-8 py-2"><strong>CLICK</strong></a>
+              <div className="bg-no-repeat border border-leaf-green rounded-xl w-5/12 ml-2 p-6" >
+                <p className="text-5xl ">APP <br/><strong>TWO</strong></p>
+                <a href="" className="text-xl text-leaf-green-60 underline hover:no-underline inline-block rounded-full mt-12 px-8 py-2"><strong>CLICK</strong></a>
               </div>
             </div>
             <div className="flex flex-row h-64 mt-6">
-              <div className="bg-blue-200 rounded-xl shadow-lg px-6 py-4 w-4/12">
+              <div className="border border-leaf-green rounded-xl shadow-lg px-6 py-4 w-4/12">
                 APP THREE
               </div>
-              <div className="bg-yellow-200 rounded-xl shadow-lg mx-6 px-6 py-4 w-4/12">
+              <div className="border border-leaf-green rounded-xl shadow-lg mx-6 px-6 py-4 w-4/12">
                 APP FOUR
               </div>
-              <div className="bg-purple-200 rounded-xl shadow-lg px-6 py-4 w-4/12">
+              <div className="border border-leaf-green rounded-xl shadow-lg px-6 py-4 w-4/12">
                 APP FIVE
               </div>
             </div>
