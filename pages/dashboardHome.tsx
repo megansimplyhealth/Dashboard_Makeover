7d07fbc4d42862ef3223c79ac866c9c7ab8718e5
Megan Knaresborough <Megan.Knaresborough@simplyhealth.co.uk>
2024-10-15 08:58:09 +0100

    basic banner done

--- pages/dashboardHome.tsx
+++ pages/dashboardHome.tsx
@@ -27,25 +27,39 @@ export default function DashboardHome() {
         </div>
 
         {/* banner */}
-        <div className="absolute grid grid-rows-3 grid-flow-col gap-4 h-28 w-full mt-16">
-          <div className="row-span-3 bg-apple-green-80 h-28">01</div>
-          <div className="row-span-2 col-span-2 bg-apple-green-60 h-28">02</div>
+        <div className="absolute grid grid-cols-4 gap-4 h-32 w-full mt-20">
+          <div className="col-span-1 h-28 p-2 ml-16 mt-1">
+            
+            <p className="text-left leading-none">
+            <strong className="text-3xl text-black">Practice news</strong><br />
+            <span className="text-xs text-grey-4 leading-none">Stay on top of all upcoming events, practice <br />revenue statistics and marketing tips.</span>
+            </p>
+
+            <div className="flex space-x-2 mt-2">
+              <button className="rounded-full border border-grey-4 text-grey-4 w-8 h-8 flex items-center justify-center" onClick={() => {}}>{"<"}</button>
+              <button className="rounded-full border border-grey-4 text-grey-4 w-8 h-8 flex items-center justify-center" onClick={() => {}}>{">"}</button>
+            </div>
+          </div>
+
+          <div className="rounded-xl col-span-3 bg-apple-green-60 h-32 flex items-center justify-center mr-16 mt-1">
+            IMAGE OF EVENTS
+          </div>
         </div>
 
         {/* search bar */}
-        <div className="absolute px-10 py-1 z-10 w-full mt-44 border-t-2 border-grey-2">
-          <div className="flex items-center justify-center py-2 text-5x1">
+        <div className="absolute px-10 py-1 z-10 w-full mt-56 border-t-2 border-grey-2">
+          <div className="flex items-center justify-center py-2 text-5x1 mt-2">
             <input
               type="text"
-              className="text-leaf-green bg-white p-2 rounded-lg w-1/2"
-              placeholder="Search..."
+              className="text-leaf-green text-center bg-white p-2 rounded-lg w-1/2"
+              placeholder="{SEARCH ICON} Search for patients, reports, settings..."
               placeholder-color="#006633"
             />
           </div>
         </div>
 
         {/* content section */}
-        <div className="flex justify-center items-center flex-row pt-64 px-10 pb-4">         
+        <div className="flex justify-center items-center flex-row pt-80 px-10 pb-4">         
           <div className="w-10/12">
             <div className="flex flex-row">
               <div className="bg-no-repeat bg-red-200 border border-red-300 rounded-xl w-7/12 mr-2 p-6" >
